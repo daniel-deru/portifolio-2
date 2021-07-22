b23dd346b1b69e691ec3e8ed792cde9ac6ffb024
daniel <danielderu@gmail.com>
2021-07-22 07:25:56 +0200

    tried to use the data from the json file to map through and create dynamic elements but the animations don't work when doing this going to switch back to the old method and create each project indivdually

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,71 +1,53 @@
 import Image from "next/image"
 
-import React from 'react'
+import React, { useEffect, useRef, useState, useLayoutEffect } from 'react'
 
 import styles from "../styles/Projects.module.css"
 
 import crypto from "../public/crypto1.png"
+import trac from "../public/protrac1.png"
+import poke from "../public/pokedex1.png"
+import paint from "../public/paint1.png"
+
+import gsap from "gsap"
+
+import { projects as projectsList } from "./projects.json"
 
 
 const projects = () => {
+    const projectsRef = useRef<HTMLElement>(null)
+    const images = [crypto, trac, poke, paint]
+
+    useLayoutEffect(() => {
+        console.log(projectsList)
+        console.log(projectsRef)
+
+        // const timeline = gsap.timeline({ defaults: {duration: 0.8}})
+        // timeline.from(projectsRef.current, {x: 50})
+                // .from(projectsRef.current.children[1], {x: -50})
+        
+    }, [])
     return (
-        <div className={styles.container}>
-            <section>
-                <Image src={crypto} layout="fill"/>
-                <div>
-                    <h3>Cryptgraph</h3>
-                    <ul>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>JavaScript</li>
-                        <li>NodeJS</li>
-                        <li>ExpressJS</li>
-                    </ul>
-                    <a href="#">More</a>
-                </div>
-                
-            </section>
-           <section>
-                <div>
-                    <h3>ProTrac</h3>
-                    <ul>
-                        <li>JavaScript</li>
-                        <li>ReactJS</li>
-                        <li>CSS</li>
-                        <li>NodeJS</li>
-                        <li>ExpressJS</li>
-                        <li>MySQL</li>
-                    </ul>
-                    <a href="#">More</a>
-                </div>
-           </section>
-           <section>
-                <div>
-                    <h3>Pokedex</h3>
-                    <ul>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>JavaScript</li>
-                        <li>NodeJS</li>
-                        <li>ExpressJS</li>
-                        <li>Axios</li>
-                    </ul>
-                    <a href="#">More</a>
-                </div>
-           </section>
-           <section>
-                <div>
-                    <h3>Paint Magic</h3>
-                    <ul>
-                        <li>ReactJS</li>
-                        <li>CSS</li>
-                        <li>JavaScript</li>
-                        <li>NodeJS</li>
-                        <li>ExpressJS</li>
-                    </ul>
-                    <a href="#">More</a>
-                </div>
-           </section>    
+        <div className={styles.container} >
+            {projectsList.map((project: object, index: number) => {
+                return  <section ref={projectsRef} className={styles.project}>
+                            <div>
+                                <Image src={images[index]} alt="" width={500} height={300} layout="responsive" />
+                            </div>
+                            <div>
+                                <h3>{project.name}</h3>
+                                <ul>
+                                    {project.stack.map((item: string) => {
+                                        return  <li>
+                                                    {item}
+                                                </li>
+                                    })}
+                                </ul>
+                                <a href="#">More...</a>
+                            </div>
+                            
+                        </section>
+            })}
         </div>
     )
 }
